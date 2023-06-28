d149694aa229a5029c467fb167215013497b2219
dhanush <dhanushprabakaran18@gmail.com>
2023-06-29 00:45:41 +0530

    changes

--- app/page.jsx
+++ app/page.jsx
@@ -50,17 +50,19 @@ const page = () => {
             <Postcard />
             <Postcard />
             <Postcard />
-            <Postcard />
+            
           </div>
         </div>
 
         <div className="col-span-4 md:col-span-1 m-3">
-          <div className="  h-screen grow flex-col bg-white bg-opacity-20 overflow-hidden overflow-y-scroll rounded-md ml-3 ">
+          <div className="  grow flex-col bg-white bg-opacity-20 overflow-hidden overflow-y-scroll rounded-md ml-3 ">
           <div
            className=" p-7 font-bold text-2xl md:col-span-3  m-2 shadow-md rounded-lg bg-white bg-opacity-50 text-center h-10 flex justify-center items-center  uppercase"
            >
             Recent Posts
            </div>
+           <Postcard />
+           <Postcard />
             {/* <Postcard /> */}
             <div
            className=" font-semibold text-1xl md:col-span-3  m-2 p-2 shadow-md rounded-lg bg-white bg-opacity-50 text-center h-10 flex justify-center items-center sticky"
