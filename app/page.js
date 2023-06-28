f4b25becee1833dc157e4650b18793425589939a
dhanush <dhanushprabakaran18@gmail.com>
2023-06-28 14:10:28 +0530

    chage in the nave bar

--- app/page.js
+++ app/page.js
@@ -2,13 +2,17 @@
 import Header from "@/components/Header";
 import Postcard from "@/components/Postcard";
 import Footer from "@components/Footer";
+import { Suspense } from "react";
+import Loading from "./Loading";
+
 const page = () => {
   return (
     <div className="md:flex md:justify-center md:h-screen ">
       <div className=" grid grid-cols-4 gap-2 w-screen md:w-[98vw] ">
         <Header />
-
-        <div className="col-span-4 md:col-span-3 flex  m-3">
+        <Suspense fallback={<Loading/>}>
+        <div className="col-span-4 md:col-span-3 flex  m-3" >
+          
            <div className="h-screen grow bg-white overflow-hidden overflow-y-scroll bg-opacity-0   items-stretch grid grid-flow-row md:grid-cols-3 justify-center "> {/*  */}
            <div
            className=" font-bold text-2xl md:col-span-3  m-2 shadow-md rounded-lg bg-white bg-opacity-50 text-center h-10 flex justify-center items-center sticky"
@@ -53,7 +57,7 @@ const page = () => {
         <div className="col-span-4 md:col-span-1 m-3">
           <div className="  h-screen grow flex-col bg-white bg-opacity-20 overflow-hidden overflow-y-scroll rounded-md ml-3 ">
           <div
-           className=" font-bold text-2xl md:col-span-3  m-2 shadow-md rounded-lg bg-white bg-opacity-50 text-center h-10 flex justify-center items-center  uppercase"
+           className=" p-7 font-bold text-2xl md:col-span-3  m-2 shadow-md rounded-lg bg-white bg-opacity-50 text-center h-10 flex justify-center items-center  uppercase"
            >
             Recent Posts
            </div>
@@ -63,8 +67,34 @@ const page = () => {
            >
              Stress 
            </div>
+           <div
+           className=" font-semibold text-1xl md:col-span-3  m-2 p-2 shadow-md rounded-lg bg-white bg-opacity-50 text-center h-10 flex justify-center items-center sticky"
+           >
+             Stress 
+           </div>
+           <div
+           className=" font-semibold text-1xl md:col-span-3  m-2 p-2 shadow-md rounded-lg bg-white bg-opacity-50 text-center h-10 flex justify-center items-center sticky"
+           >
+             Stress 
+           </div>
+           <div
+           className=" font-semibold text-1xl md:col-span-3  m-2 p-2 shadow-md rounded-lg bg-white bg-opacity-50 text-center h-10 flex justify-center items-center sticky"
+           >
+             Stress 
+           </div>
+           <div
+           className=" font-semibold text-1xl md:col-span-3  m-2 p-2 shadow-md rounded-lg bg-white bg-opacity-50 text-center h-10 flex justify-center items-center sticky"
+           >
+             Stress 
+           </div>
+           <div
+           className=" font-semibold text-1xl md:col-span-3  m-2 p-2 shadow-md rounded-lg bg-white bg-opacity-50 text-center h-10 flex justify-center items-center sticky"
+           >
+             Stress 
+           </div>
           </div>
         </div>
+        </Suspense>
         <Footer />
       </div>
     </div>
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,5 @@
 "use client"
-import { useRef } from 'react';
+import { useState } from 'react';
 import Icon from "@/public/download.png";
 import Image from "next/image";
 import Link from "next/link";
@@ -7,36 +7,43 @@ import bar from "@public/bar.svg"
 
 
 const Header = () => {
-    const ref = useRef();
-    function handleClick() {
-        //alert('increment like count');
-        ref.current.classList.add("flex");
-      }
+  const [navBar, setNavBar] = useState(false);
+  
   return (
     <div className=" relative col-span-4 bg-black bg-opacity-20 flex  justify-between items-center h-24 md:rounded-md">
-      <Image src={Icon} className="w-[20vh] h-fit ml-3" alt="hello" />
+      <Image src={Icon} className="w-[20vh] h-fit " alt="hello" />
       <div className="text-2xl font-extrabold font-mono  ">Blogs</div>
       <div
-      onClick={handleClick}>
+      onClick={()=>{setNavBar(!navBar)}}>
       <Image
       src={bar}
-      className=" w-12 mr-4" 
+      className=" w-12 " 
       alt="hello"
       />
       </div>
-      <div ref={ref} className="hidden  text-white items-end absolute  text-lg font-bold font-mono p-5 flex-col right-0  top-[12vh] w-[400px] bg-black bg-opacity-20 rounded-sm ">
-        <Link href={"/"} className="m-2 hover:text-blue-400">
+      <div id='list' className={` ${navBar? 'flex':'hidden'} items-stretch  text-white  absolute  text-lg font-bold font-mono p-5 flex-col right-0  top-[12vh] w-[400px] bg-black bg-opacity-20 rounded-sm `}>
+       
+        <Link href={"/"} className="m-2 text-end p-5 hover:bg-slate-500 rounded-md cursor-pointer z-10  ">
           link1
         </Link>
-        <Link href={"/"} className="m-2 hover:text-blue-400">
+        <Link href={"/"} className="m-2 text-end p-5 hover:bg-slate-400 rounded-md cursor-pointer z-10   ">
+          link1
+        </Link>
+        <Link href={"/"} className="m-2 text-end p-5 hover:bg-slate-500 rounded-md cursor-pointer  z-10  ">
+          link1
+        </Link>
+        <Link href={"/post"} className="m-2 text-end p-5 hover:bg-slate-500 rounded-md cursor-pointer  z-10  ">
+          link1
+        </Link>
+        {/* <Link href={"/"} className="m-2  text-end p-5 hover:bg-slate-500 rounded-md cursor-pointer">
           link2
         </Link>
-        <Link href={"/"} className="m-2 hover:text-blue-400">
+        <Link href={"/"} className="m-2  text-end p-5 hover:bg-slate-500 rounded-md">
           link3
         </Link>
-        <Link href={"/post"} className="m-2 hover:text-blue-400">
+        <Link href={"/post"} className="m-2  text-end p-5 hover:bg-slate-500 rounded-md">
           link4
-        </Link>
+        </Link> */}
       </div>
     </div>
   );
